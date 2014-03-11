89ef18133003b32a10bc873cc7ba4e8101e5f8d7
Michele Gozzi <michelegozzi@gmail.com>
2014-03-11 08:49:16 -0700

    reinitialized

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
+      user.nickname = auth.info.nickname unless auth.info.nickname.nil?
+      user.image = auth.info.image unless auth.info.image.nil?
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at) unless auth.credentials.expires_at.nil?
       user.save!
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140309155511) do
+ActiveRecord::Schema.define(version: 20140310195709) do
 
   create_table "users", force: true do |t|
     t.string   "provider"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20140309155511) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "nickname"
+    t.string   "image"
   end
 
 end
